9852b7b0503098b95ee5a1e2061d29dcffe4172f
aaramirez <aaramirez9@gmail.com>
2024-01-10 23:25:38 -0500

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aaramirez9/godesde0
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/aaramirez9/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
